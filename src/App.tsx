27bd74588c2b283baed5feac69ffa380a130e78c
Kageyama R <kageyama.ryoma@aibos2100.onmicrosoft.com>
2024-08-15 23:29:16 +0900

    second commit

--- src/App.tsx
+++ src/App.tsx
@@ -4,27 +4,52 @@ import Form from "./form"
 import Address from './address'
 import axios from "axios";
 
+interface ResponseAddress{
+  address1: string;
+  address2: string;
+  address3: string;
+  zipcode: string;
+}
+
+interface Response{
+  results: ResponseAddress[];
+  status: number;
+}
 
 function App() {
   const [value, setValue] = useState("")
-  const getAddress = async (url: string) => {
-    return await axios.get(url).then((response) => console.log(response.data));
+  const [town, setTown] = useState("")
+  const [city, setCity] = useState("")
+  const [prefec, setPrefec] = useState("")
+  const setAddress = (prefec: string, city: string, town: string) => {
+    setTown(town);
+    setCity(city);
+    setPrefec(prefec)
+  }
+  const getAddress = async (url: string)  => {
+    return await axios.get<Response>(url).then(
+      (res) => setAddress(
+        res.data.results[0].address1,
+        res.data.results[0].address2,
+        res.data.results[0].address3
+      )
+    )
   }
   const submit = (e: any) => {
     e.preventDefault();
     if(value.length == 7){
       let url = "https://zipcloud.ibsnet.co.jp/api/search?zipcode="
       url += value
-      const address = getAddress(url)
-      // console.log(address[0].address1)
+      getAddress(url)
     }
+    console.log(town)
   }
 
   return (
     <>
     <Form setValue={setValue} />
     <button onClick={submit}>検索</button>
-    <Address value={value} />
+    <Address town={town} city={city} prefec={prefec} />
     </>
   )
 }
--- src/address.tsx
+++ src/address.tsx
@@ -1,11 +1,18 @@
 
 type Props = {
-    value: string
+    town: string
+    city: string
+    prefec: string
 }
 
 const Address = (props: Props) => {
     return (
         <>
+        <div>
+            <p>{props.prefec}</p>
+            <p>{props.city}</p>
+            <p>{props.town}</p>
+        </div>
         </>
     )
 }
